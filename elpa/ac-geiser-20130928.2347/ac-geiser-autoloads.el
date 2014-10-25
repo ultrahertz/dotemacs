5d32c9d0e09d48268515a36ce5a118e206fd62d4
James F <jamesfbsd@gmail.com>
2014-10-25 10:39:48 +0100

    Initial commit'

---
+++ elpa/ac-geiser-20130928.2347/ac-geiser-autoloads.el
@@ -0,0 +1,33 @@
+;;; ac-geiser-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (ac-geiser-setup) "ac-geiser" "ac-geiser.el" (21579
+;;;;;;  21293 401872 161000))
+;;; Generated autoloads from ac-geiser.el
+
+(defvar ac-source-geiser '((candidates . ac-source-geiser-candidates) (symbol . "g") (document . ac-geiser-documentation)) "\
+Source for geiser completion")
+
+(autoload 'ac-geiser-setup "ac-geiser" "\
+Add the geiser completion source to the front of `ac-sources'.
+This affects only the current buffer.
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("ac-geiser-pkg.el") (21579 21293 466420
+;;;;;;  832000))
+
+;;;***
+
+(provide 'ac-geiser-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; ac-geiser-autoloads.el ends here
